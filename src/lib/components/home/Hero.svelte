de5d8abd02eb26c59e7995f0b62585ac4d0e64a2
imlargo <jlargob@unal.edu.co>
2025-01-26 08:17:48 -0500

    add Hero component for displaying a full-screen hero image on the home page

---
+++ src/lib/components/home/Hero.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+const imgSource = "https://s3-alpha-sig.figma.com/img/0b61/5b62/97a056e67a0e42203ed734ace25c3522?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GnBuvRv8R0N5kFe5Tnl9iK00hEV7KLXVC6jm5p0ZOkhNya8JPkGg-zyspWK1tyhzqdhGvqAHQIvABqU5mHdxZILSjrtD04qr4cUvDFr5B7rk82U0r44SzgVJHp4wWeid9zo4CuByk50~L561OJNvzUqACUTPLNz61pv5VN07jROf2Q-g42AVSZdQ7CupZGGT-2zvoD~CFSmFo~oB-aUIAR8HkQpaB1fM1KfDNQ4bbUJuYEXanV9dIzjI8W-Xb2ioNgOcMOyulDzcPK4Xw8~Ue08g2uSQODihCRruldACLvEtBTgOR9MyynBmbdf0-XNi2oBQWkxeQiAMAPR-1w5kYw__"
+</script>
+
+<div>
+
+
+    <img src={imgSource} class="w-full h-screen object-cover" alt="hero" />
+</div>
