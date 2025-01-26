459ad390922f3403dc8a92c1118c19bc431c59e0
imlargo <jlargob@unal.edu.co>
2025-01-26 08:17:53 -0500

    add Hero and Galery components to the home page for enhanced visual presentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Galery from "$lib/components/galery/Galery.svelte";
+    import Hero from "$lib/components/home/Hero.svelte";
+</script>
+
+<Hero />
+
+
+<div class="space-y-12">
+    <div>
+        <Galery title="Para ti" />
+    </div>
+
+    <div>
+        <Galery title="Para ti" />
+    </div>
+
+    <div>
+        <Galery title="Para ti" />
+    </div>
+</div>
