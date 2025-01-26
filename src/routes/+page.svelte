e03b78153f7947632f718a0ead746f57faf0a1fa
imlargo <jlargob@unal.edu.co>
2025-01-26 08:59:45 -0500

    feat: Integrate GaleryEvento component and reorganize gallery sections for enhanced event display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
     import Galery from "$lib/components/galery/Galery.svelte";
+    import GaleryEvento from "$lib/components/galery/GaleryEvento.svelte";
     import Hero from "$lib/components/home/Hero.svelte";
 </script>
 
 <Hero />
 
-
-<div class="space-y-12">
-    <div>
+<div class="space-y-24 mt-24">
         <Galery title="Para ti" />
-    </div>
-
-    <div>
-        <Galery title="Para ti" />
-    </div>
-
-    <div>
-        <Galery title="Para ti" />
-    </div>
+        <GaleryEvento title="Eventos" />
+        <Galery title="Recomendados" />
+        <Galery title="Nuevas experiencias" />
 </div>
