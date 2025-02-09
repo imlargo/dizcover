7d6cab2e5052fac298c1f33cbed237f52b2de1f6
imlargo <jlargob@unal.edu.co>
2025-02-09 09:00:03 -0500

    fix: Update image URL in Hero component for improved accessibility

--- src/lib/components/home/Hero.svelte
+++ src/lib/components/home/Hero.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	const imgSource =
-		'https://s3-alpha-sig.figma.com/img/0b61/5b62/97a056e67a0e42203ed734ace25c3522?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GnBuvRv8R0N5kFe5Tnl9iK00hEV7KLXVC6jm5p0ZOkhNya8JPkGg-zyspWK1tyhzqdhGvqAHQIvABqU5mHdxZILSjrtD04qr4cUvDFr5B7rk82U0r44SzgVJHp4wWeid9zo4CuByk50~L561OJNvzUqACUTPLNz61pv5VN07jROf2Q-g42AVSZdQ7CupZGGT-2zvoD~CFSmFo~oB-aUIAR8HkQpaB1fM1KfDNQ4bbUJuYEXanV9dIzjI8W-Xb2ioNgOcMOyulDzcPK4Xw8~Ue08g2uSQODihCRruldACLvEtBTgOR9MyynBmbdf0-XNi2oBQWkxeQiAMAPR-1w5kYw__';
+		'https://s3-alpha-sig.figma.com/img/0b61/5b62/97a056e67a0e42203ed734ace25c3522?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=cXRFWze1EznZ1qOO8FlJnfFD9sTO86W3Ig6BXK5tM69RWW2al2mtm7mFuUskc3QfRzIRdDVyw40dWtZ7kJ3v16bSbPdzaITr0He77kcjDMNCtw~EXbYiifbuwgH0t9mXGbGyXLtapOSKNt777~ReYL963OTKqvUCnjRYofZdXpg5pxFGVEQBguVsfK-TKAHgaj2NXfzfLw7Eqr8R5yI-uOIVu8xN4ofs8fBNXzGd-H7uStoQJ4yVmi3TBnPF8u-AvSQ1RAdClNDcom3-TmYRFoVG8989976ZYhjBFAMtvZtqxku3MlfWiWCtzXXpVKmTV3UnzDlmVAk4AkVsGrgRww__';
 
 	const establecimiento = {
 		nombre: 'Dulcinea',
