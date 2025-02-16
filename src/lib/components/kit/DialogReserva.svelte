e3ba6a3674c44128e103ee3f3127cbf10e4e4b4b
imlargo <jlargob@unal.edu.co>
2025-02-16 09:17:56 -0500

    feat: Add dialog component with customizable header, footer, and overlay

---
+++ src/lib/components/kit/DialogReserva.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import * as Dialog from '$lib/components/ui/dialog';
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger>Reservar</Dialog.Trigger>
+	<Dialog.Content>
+		<Dialog.Header>
+			<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+			<Dialog.Description>
+				This action cannot be undone. This will permanently delete your account and remove your data
+				from our servers.
+			</Dialog.Description>
+		</Dialog.Header>
+	</Dialog.Content>
+</Dialog.Root>
