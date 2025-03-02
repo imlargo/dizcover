a9ca8fdedcec8c941da657d51f23f054291b10dd
imlargo <jlargob@unal.edu.co>
2025-03-02 07:33:58 -0500

    feat: Add NumberField component for incrementing and decrementing values

---
+++ src/lib/components/kit/NumberField.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	type Props = {
+		min: number;
+		max: number;
+		value: number;
+		onChange: (value: number) => void;
+	};
+
+	let { min, max, value = $bindable(), onChange }: Props = $props();
+
+	const increment = () => {
+		if (value < max) {
+			value += 1;
+			onChange(value);
+		}
+	};
+
+	const decrement = () => {
+		if (value > min) {
+			value -= 1;
+			onChange(value);
+		}
+	};
+</script>
+
+<div class="flex items-center space-x-3 rounded">
+	<Button onclick={decrement} variant="ghost" class="flex items-center jutify-center size-8 rounded-full border">
+		<i class="bi bi-dash leading-none"></i>
+	</Button>
+	
+    <span>{value}</span>
+
+	<Button onclick={increment} variant="ghost" class="flex items-center jutify-center size-8 rounded-full border">
+		<i class="bi bi-plus leading-none"></i>
+	</Button>
+</div>
