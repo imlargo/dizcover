ab3f2dbaa38a27e63be500524e584897b574e996
imlargo <jlargob@unal.edu.co>
2025-03-02 08:13:16 -0500

    feat: Add Artista component for displaying artist information with image and name

---
+++ src/lib/components/evento/Artista.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    type Props = {
+        imagen: string;
+        nombre: string;
+    }
+
+    const { imagen, nombre }: Props = $props();
+</script>
+
+
+<div class="flex flex-col gap-4">
+    <div class="size-56 rounded-full overflow-hidden">
+        <img src={imagen} alt={nombre} class="size-56 object-cover rounded-full bg-neutral-700 hover:scale-110 transition-all duration-300" />
+
+    </div>
+
+    <div class="flex items-center justify-center">
+        <span class="text-xl font-semibold">{nombre}</span>
+    </div>
+</div>
