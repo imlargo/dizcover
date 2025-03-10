80880002ec6b22cd83005462e5c82c186f56e017
imlargo <jlargob@unal.edu.co>
2025-03-10 15:30:43 -0500

    feat: Add variant prop to EventCard for conditional rendering of event details

--- src/lib/components/home/galery/cards/CardEvento.svelte
+++ src/lib/components/home/galery/cards/CardEvento.svelte
@@ -2,25 +2,29 @@
 	import { Badge } from '$components/ui/badge/index';
 	import type { Evento } from '$lib/types/evento';
 
+	type EventCardVariant = 'base' | 'cover';
+
 	type Props = {
 		evento: Evento;
+		variant?: EventCardVariant;
 	};
 
-	const { evento }: Props = $props();
+	const { evento, variant = 'base' }: Props = $props();
 </script>
 
-<div class="flex flex-col items-center justify-center w-[315px]">
-	<div class="overflow-hidden rounded-md h-[466px] w-[315px]">
+<div class="flex w-[315px] flex-col items-center justify-center">
+	<div class="h-[466px] w-[315px] overflow-hidden rounded-md">
 		<a data-sveltekit-reload href="/evento/{evento.id}" class="overflow-hidden rounded-md">
 			<img src={evento.primera_imagen} alt="" class="h-full w-full rounded-md object-cover" />
 		</a>
 	</div>
-	
 
-	<div class="text-center">
-		<h5 class="mt-4 text-xl font-bold">{evento.nombre}</h5>
-		<span class="block">{evento.fecha}</span>
-		<span class="block">{evento.lugar}</span>
-		<span class="block">{evento.hora?.inicio} - {evento.hora?.fin}</span>
-	</div>
+	{#if variant !== 'cover'}
+		<div class="text-center">
+			<h5 class="mt-4 text-xl font-bold">{evento.nombre}</h5>
+			<span class="block">{evento.fecha}</span>
+			<span class="block">{evento.lugar}</span>
+			<span class="block">{evento.hora?.inicio} - {evento.hora?.fin}</span>
+		</div>
+	{/if}
 </div>
