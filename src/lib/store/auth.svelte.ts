435149171d828be006a2ddda87ac4dec869ab800
imlargo <jlargob@unal.edu.co>
2025-03-16 08:32:41 -0500

    feat: Refactor profile routes to separate user-specific invoices and favorites, and implement new layout structure

--- src/lib/store/auth.svelte.ts
+++ src/lib/store/auth.svelte.ts
@@ -1,7 +1,7 @@
 import type { User } from '$lib/types/models/user';
 
 class AuthStore {
-	accessToken = $state('');
+	accessToken = $state('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzQyMTM1NDU4LCJpYXQiOjE3NDIxMzE4NTgsImp0aSI6ImQxOTM5NjBlMGY0NTQ5YTU5MGIwODg1MmJkODRmM2JkIiwidXNlcl9pZCI6MjF9.RtSrpFjHWTNGUmCdEbQ74hXZKrisDFu86I9bYBfVd1w');
 	refreshToken = $state('');
 	user: User | null = $state(null);
 
