e9375b2fa3f4129d50daa06a256218d28b630b0a
imlargo <jlargob@unal.edu.co>
2025-03-16 07:57:55 -0500

    feat: Extend Locals interface to include accessToken, refreshToken, and user properties

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,19 @@
+import type { User } from "$lib/types/models/user";
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			accessToken: string;
+			refreshToken: string;
+			user: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export { };
