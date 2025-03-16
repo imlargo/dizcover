e668d2e48e52b42ec0b60543507b158baddb043b
imlargo <jlargob@unal.edu.co>
2025-03-16 08:06:16 -0500

    feat: Enhance User model with additional fields and Fiestero type definition

--- src/lib/types/models/user.ts
+++ src/lib/types/models/user.ts
@@ -1,4 +1,17 @@
+type Fiestero = {
+    id: number;
+    identidad_sexo: string;
+    num_identificacion: string;
+    pasaporte: string;
+    user: number;
+}
+
 export type User = {
     id: number;
-    email: string; 
+    nombre_usuario: string;
+    email: string;
+    nombre_completo: string;
+    foto_perfil: string | null;
+    tipo: string;
+    fiestero: Fiestero;
 }
