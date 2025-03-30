64a0652b8dd8d2c75db09745b4b0e57c164f62d1
imlargo <jlargob@unal.edu.co>
2025-03-30 10:04:42 -0500

    feat: Enhance geo page with custom markers and improved map zoom level

--- src/routes/geo/+page.svelte
+++ src/routes/geo/+page.svelte
@@ -16,13 +16,33 @@
 
 		map = L.map(element).setView(
 			[storeLocation.location.lat as number, storeLocation.location.lng as number],
-			13
+			16
 		);
 
 		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 19,
+			maxZoom: 21,
 			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 		}).addTo(map);
+
+        const locations = [
+            { lat: 6.2095588, lng: -75.5659474, nombre: "Establecimiento 1" },
+            { lat: 6.2097457, lng: -75.5659753, nombre: "Establecimiento 2" },
+            { lat: 6.2108964, lng: -75.5711483, nombre: "Establecimiento 3" },
+        ]
+
+        const marketIcon = L.icon({
+            iconUrl: 'marker.svg',
+            iconSize: [40, 40],
+            iconAnchor: [20, 40],
+            popupAnchor: [0, -40]
+        });
+
+        for (const location of locations) {
+            const marker = L.marker([location.lat, location.lng], {
+                icon: marketIcon
+            }).addTo(map);
+            marker.bindPopup(location.nombre).openPopup();
+        }
 	}
 
 	onMount(() => {
@@ -37,7 +57,6 @@
 </script>
 
 <main>
-	<button onclick={storeLocation.requestLocation}>Location</button>
 	<div class="map" bind:this={mapElement}></div>
 </main>
 
