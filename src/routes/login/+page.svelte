f1a6d0fd0a96c0c397ca09dff2850ada8f3e0a14
imlargo <jlargob@unal.edu.co>
2025-04-05 22:46:06 -0500

    feat: Simplify OAuth sign-in process by redirecting to API endpoint

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -9,6 +9,7 @@
 	import { onMount } from 'svelte';
 	import { invalidateAll } from '$app/navigation';
 	import { toast } from 'svelte-sonner';
+	import { apiBaseUrl } from '$lib/constants';
 
 	type LoginForm = {
 		email: string;
@@ -54,36 +55,7 @@
 	 * Create form to request access token from Google's OAuth 2.0 server.
 	 */
 	function oauthSignIn() {
-		// Google's OAuth 2.0 endpoint for requesting an access token
-		const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-		// Create <form> element to submit parameters to OAuth 2.0 endpoint.
-		const form = document.createElement('form');
-		form.setAttribute('method', 'GET'); // Send as a GET request.
-		form.setAttribute('action', oauth2Endpoint);
-
-		// Parameters to pass to OAuth 2.0 endpoint.
-		const params: Record<string, string> = {
-			client_id: '598816663524-2jkeue6coob8m59nkqn288161n5uqdkp.apps.googleusercontent.com',
-			redirect_uri: `http://127.0.0.1:8000/accounts/google/login/callback/`,
-			response_type: 'code',
-			prompt: 'select_account',
-			scope: 'openid profile email',
-			include_granted_scopes: 'true'
-		};
-
-		// Add form parameters as hidden input values.
-		for (const p in params) {
-			const input = document.createElement('input');
-			input.setAttribute('type', 'hidden');
-			input.setAttribute('name', p);
-			input.setAttribute('value', params[p]);
-			form.appendChild(input);
-		}
-
-		// Add form to page and submit it to open the OAuth 2.0 endpoint.
-		document.body.appendChild(form);
-		form.submit();
+		window.location.href = `${apiBaseUrl}/accounts/google/login`;
 	}
 
 	onMount(async () => {
