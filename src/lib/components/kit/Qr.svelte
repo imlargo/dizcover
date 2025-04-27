2cbb4ca0fe231ae0b0dc7cc0c4da4e5d8e830b79
imlargo <jlargob@unal.edu.co>
2025-04-27 09:55:16 -0500

    feat: Add QR code generation and display in user reservations page

---
+++ src/lib/components/kit/Qr.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import QRCode from 'qrcode';
+
+	type Props = {
+		content: string;
+        size?: "sm" | "md" | "lg" | "xl";
+		class?: string;
+	};
+
+	const { content, class: className = '', size = "md"  }: Props = $props();
+
+    const sizes = {
+        sm: 128,
+        md: 192,
+        lg: 256,
+        xl: 320,
+    }
+
+    function useCanvasQr(element: HTMLCanvasElement) {
+        QRCode.toCanvas(element, content, {
+            width: sizes[size],
+        })
+    }
+</script>
+
+<canvas class={className} use:useCanvasQr id="canvas"></canvas>
---
+++ src/routes/(authed)/profile/[user]/reservations/+page.server.ts
@@ -0,0 +1,12 @@
+import { DatabaseController } from '$lib/services/db';
+import type { PageServerLoad } from './$types';
+
+export const load = (async ({ locals }) => {
+
+    const db = new DatabaseController(locals.accessToken);
+    const establecimiento = await db.getEstablecimiento(2);
+
+    return {
+        establecimiento
+    };
+}) satisfies PageServerLoad;
