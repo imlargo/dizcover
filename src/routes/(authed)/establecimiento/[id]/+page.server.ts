bd820ab7373e758c1bb56ada55e32ea8c59cc827
imlargo <jlargob@unal.edu.co>
2025-05-04 08:21:55 -0500

    feat: Integrate Cupon functionality into GalerySale component and update page structure

--- src/routes/(authed)/establecimiento/[id]/+page.server.ts
+++ src/routes/(authed)/establecimiento/[id]/+page.server.ts
@@ -1,15 +1,19 @@
 import type { PageServerLoad } from './$types';
 import { DatabaseController } from '$lib/services/db';
+import { CuponController } from '$lib/controllers/cupon';
 
 export const load = (async ({ params, locals }) => {
 	const dbController = new DatabaseController(locals.accessToken);
 
-	const [establecimiento, eventos, imagenes, cordenadas, horarios] = await Promise.all([
+
+	const cuponController = new CuponController(locals.accessToken);
+	const [establecimiento, eventos, imagenes, cordenadas, horarios, cupones] = await Promise.all([
 		dbController.getEstablecimiento(params.id),
 		dbController.getEventosEstablecimiento(params.id),
 		dbController.getImagenesEstablecimiento(parseInt(params.id)),
 		dbController.getCordenadasEstablecimiento(parseInt(params.id)),
-		dbController.getHorariosEstablecimiento(parseInt(params.id))
+		dbController.getHorariosEstablecimiento(parseInt(params.id)),
+		cuponController.getCuponesEstablecimiento(parseInt(params.id))
 	]);
 
 	return {
@@ -17,6 +21,7 @@ export const load = (async ({ params, locals }) => {
 		eventos: eventos,
 		imagenes: imagenes,
 		cordenadas: cordenadas,
-		horarios: horarios
+		horarios: horarios,
+		cupones: cupones
 	};
 }) satisfies PageServerLoad;
