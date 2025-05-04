0f73b1e2cc127d270ad2a2ee28390c8a8fd12e82
imlargo <jlargob@unal.edu.co>
2025-05-04 08:21:31 -0500

    feat: Implement CuponController and define Cupon model and response interface

---
+++ src/lib/controllers/cupon.ts
@@ -0,0 +1,41 @@
+import api from "$lib/services/api";
+import type { Cupon, CuponesEstablecimientoResponse } from "$lib/types/models/cupon";
+import { createFormData } from "$lib/utils/tools";
+
+export class CuponController {
+    token?: string;
+
+    constructor(token?: string) {
+        this.token = token;
+    }
+
+    async createCupon(id_establecimiento: number, cupon: any) {
+        return await api.post(`/api/establecimiento/v1/cupones/${id_establecimiento}`, {
+            body: createFormData(cupon),
+        }, {}, this.token)
+    }
+
+    async getCuponesEstablecimiento(id_establecimiento: number) {
+        return await api.get<CuponesEstablecimientoResponse>(`/api/establecimiento/v1/cupones/${id_establecimiento}`, {}, this.token)
+    }
+
+    async getCupon(id_cupon: number) {
+        return await api.get<Cupon>(`/api/establecimiento/v1/cupon/${id_cupon}`, {}, this.token)
+    }
+
+    async reclamarCupon(id_cupon: number) {
+        return await api.post(`/api/establecimiento/v1/consumo_cupon/`, {
+            body: {
+                cupon_id: id_cupon,
+            },
+        }, {}, this.token)
+    }
+
+    async validarCupon(codigo: string) {
+        return await api.post(`/api/establecimiento/v1/consumo_cupon/validar/`, {
+            body: {
+                codigo: codigo,
+            },
+        }, {}, this.token)
+    }
+}
