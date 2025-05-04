5b8815d558dd6d0dde5a64c1607e557c86e87ba4
imlargo <jlargob@unal.edu.co>
2025-05-04 09:03:11 -0500

    feat: Enhance Cupon page with detailed information display and server-side loading

--- src/routes/(authed)/cupon/[id]/+page.svelte
+++ src/routes/(authed)/cupon/[id]/+page.svelte
@@ -1,5 +1,64 @@
 <script lang="ts">
-    import type { PageData } from './$types';
+	import { formatCurrency, formatDate } from '$lib/utils/tools';
+	import type { PageData } from './$types';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import type { Cupon } from '$lib/types/models/cupon';
+	import type { Establecimiento } from '$lib/types/establecimiento';
+	import { Button } from '$lib/components/ui/button/index.js';
 
-    let { data }: { data: PageData } = $props();
+	let { data }: { data: PageData } = $props();
+
+	const cupon = data.cupon as Cupon;
+	const establecimiento = data.establecimiento as Establecimiento;
 </script>
+
+<main class="space-y-4 bg-neutral-900 py-8">
+	<h2 class="font-display text-center text-2xl font-semibold">Cupon de consumo</h2>
+
+	<div class="grid grid-cols-3 gap-12 rounded-lg p-8 place-items-center">
+		<div class="space-y-8">
+			<div class="space-y-4">
+				<h3 class="font-display text-3xl">{establecimiento.nombre}</h3>
+				<div class="flex flex-wrap gap-2">
+					{#each establecimiento.etiquetas || [] as tag}
+						<Badge variant="purple">{tag}</Badge>
+					{/each}
+				</div>
+			</div>
+
+			<p class="max-w-prose text-xl">
+				<span class="font-display">Descripcion: </span>
+				{cupon?.descripcion}
+			</p>
+
+			<p class="max-w-prose text-xl">
+				<span class="font-display">Terminos y condiciones: </span>
+				<span>Requisito mínimo de personas: {cupon.minimo_personas}</span>
+			</p>
+
+			<p class="max-w-prose text-xl">
+				<span class="font-display">Fecha de vigencia: </span>
+				{formatDate(cupon?.fecha_vigencia?.desde)} - {formatDate(cupon?.fecha_vigencia?.hasta)}
+			</p>
+		</div>
+
+		<div class="flex justify-end">
+			<div class="max-w-96 space-y-12 rounded-lg bg-neutral-500/80 p-6">
+				<img class="w-full rounded-lg" alt="" src={cupon.imagen} />
+				<div class="flex items-center justify-between">
+					<p class="font-display text-2xl">Descuento</p>
+					<p class="font-display text-2xl">%{formatCurrency(cupon.porcentaje_descuento)}</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex w-full justify-end">
+			<div class="max-w-max space-y-2">
+				<Button variant="outline" class="max-w-max p-8 font-semibold text-black">Reclamar</Button>
+				<p class="max-w-96 text-muted-foreground">
+					*Una vez reclamado el cupón, no usarlo puede incurrir en penalizaciones
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
