20491ad0f0cb284098b65f6691b469c8ec67de25
imlargo <jlargob@unal.edu.co>
2025-05-18 10:14:37 -0500

    feat: Add star icon to buttons and improve layout for establecimiento detail page

--- src/routes/(discotequero)/dashboard/establecimiento/[id]/+page.svelte
+++ src/routes/(discotequero)/dashboard/establecimiento/[id]/+page.svelte
@@ -5,7 +5,7 @@
 
 	const establecimiento = $state(data.establecimiento);
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { Pencil, Trash, View } from 'lucide-svelte';
+	import { Pencil, Star, Trash, View } from 'lucide-svelte';
 </script>
 
 <main class="space-y-4">
@@ -27,7 +27,53 @@
 		</div>
 	</div>
 
-	<div class="w-full aspect-[3/1] overflow-hidden rounded-lg">
-		<img src={establecimiento.primera_imagen} alt={establecimiento.nombre} class="w-full h-auto object-cover" />
+	<div class="aspect-[3/1] w-full overflow-hidden rounded-lg">
+		<img
+			src={establecimiento.primera_imagen}
+			alt={establecimiento.nombre}
+			class="h-auto w-full object-cover"
+		/>
+	</div>
+
+	<div class="grid grid-cols-2 gap-4">
+		<div class="relative flex items-center justify-center rounded-lg bg-neutral-900 p-8">
+			<div class="space-y-2 text-center">
+				<p class="font-display text-4xl">$3.500.000</p>
+				<p class="font-medium text-neutral-400">Total ingresos</p>
+			</div>
+
+			<div class="absolute bottom-0 right-0 p-4">
+				<span class="flex items-center gap-1 font-bold text-green-500">
+					<span>100%</span>
+				</span>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-2 gap-8 rounded-lg bg-neutral-900 p-8">
+			<div class="space-y-2">
+				<p class="font-display text-3xl">
+					{establecimiento.calificacion_promedio.toFixed(1)}
+				</p>
+				<p class="font-medium text-neutral-400 flex gap-1 items-center">
+					
+					<span>Calificación</span>
+				</p>
+			</div>
+
+			<div class="space-y-2">
+				<p class="font-display text-3xl">300</p>
+				<p class="font-medium text-neutral-400">Boletas vendidas</p>
+			</div>
+
+			<div class="space-y-2">
+				<p class="font-display text-3xl">123</p>
+				<p class="font-medium text-neutral-400">Visualizaciones</p>
+			</div>
+
+			<div class="space-y-2">
+				<p class="font-display text-3xl">742</p>
+				<p class="font-medium text-neutral-400">Cupones vendidos</p>
+			</div>
+		</div>
 	</div>
 </main>
