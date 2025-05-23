0933740dfa291d9efc366922eef145b577b2641d
imlargo <jlargob@unal.edu.co>
2025-05-23 14:39:38 -0500

    improve layout

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import type { LayoutData } from './$types';
+	import Navbar from '$components/layout/Navbar.svelte';
+	import Footer from '$components/layout/Footer.svelte';
+	let { data, children }: { data: LayoutData; children: Snippet } = $props();
+</script>
+
+<Navbar />
+
+{@render children()}
+
+<Footer />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,10 @@
 	import '$styles/app.scss';
 	let { children } = $props();
 
-	import Navbar from '$components/layout/Navbar.svelte';
-	import Footer from '$components/layout/Footer.svelte';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 </script>
 
-<Navbar />
 
 {@render children()}
 
-<Footer />
 <Toaster />
