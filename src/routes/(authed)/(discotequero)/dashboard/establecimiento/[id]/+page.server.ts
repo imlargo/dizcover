ca20f862d59987aa7304a9f73e36c16c0c82a7d4
imlargo <jlargob@unal.edu.co>
2025-05-23 14:38:19 -0500

    fix

--- src/routes/(authed)/(discotequero)/dashboard/establecimiento/[id]/+page.server.ts
+++ src/routes/(authed)/(discotequero)/dashboard/establecimiento/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import { DatabaseController } from '$lib/services/db';
-import type { PageServerLoad } from '../$types';
+import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, locals }) => {
 	const db = new DatabaseController(locals.accessToken);
