5b66d77fb117a5ce9e614d9b561e62d8f9e31c01
imlargo <jlargob@unal.edu.co>
2025-06-08 07:43:31 -0500

    fix: handle cordenadas retrieval in establecimiento page load

--- src/routes/(authed)/(fiestero)/establecimiento/[id]/+page.server.ts
+++ src/routes/(authed)/(fiestero)/establecimiento/[id]/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from './$types';
 import { DatabaseController } from '$lib/services/db';
 import { CuponController } from '$lib/controllers/cupon';
 import { ReviewsController } from '$lib/controllers/reviews';
+import type { CoordsEstablecimiento } from '$lib/types/establecimiento';
 
 export const load = (async ({ params, locals }) => {
 	const dbController = new DatabaseController(locals.accessToken);
@@ -9,12 +10,18 @@ export const load = (async ({ params, locals }) => {
 	const cuponController = new CuponController(locals.accessToken);
 	const reviewController = new ReviewsController(locals.accessToken);
 
-	const [establecimiento, eventos, imagenes, cordenadas, horarios, cupones, reviews] =
+	let cordenadas: CoordsEstablecimiento[] = [];
+	try {
+		cordenadas = await dbController.getCordenadasEstablecimiento(parseInt(params.id));
+	} catch (error) {
+
+	}
+
+	const [establecimiento, eventos, imagenes, horarios, cupones, reviews] =
 		await Promise.all([
 			dbController.getEstablecimiento(params.id),
 			dbController.getEventosEstablecimiento(params.id),
 			dbController.getImagenesEstablecimiento(parseInt(params.id)),
-			dbController.getCordenadasEstablecimiento(parseInt(params.id)),
 			dbController.getHorariosEstablecimiento(parseInt(params.id)),
 			cuponController.getCuponesEstablecimiento(parseInt(params.id)),
 			reviewController.getReviewsEstablecimiento(parseInt(params.id))
